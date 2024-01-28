b357461806dfd66c396bf6b113c1805bce89c164
thanhtrairo <tienthanhcntn789@gmail.com>
2024-01-28 17:22:14 +0700

    update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,67 +1,72 @@
 'use client'
 
 import React from 'react'
+import { motion } from 'framer-motion'
 
 import SectionHeading from './SectionHeading'
-// import { experiencesData } from '~/lib/data'
+import { experiencesData } from '~/lib/data'
 import { useSectionInView } from '~/lib/hooks'
-// import { THEME, useThemeContext } from '~/contexts/ThemeContext'
 
 const Experience = () => {
   const { ref } = useSectionInView('Experience')
-  // const { theme } = useThemeContext()
+
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      x: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  }
 
   return (
     <section id="experience" ref={ref} className="mb-28 scroll-mt-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
-        <li className="mb-10 ml-4">
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Application UI code in Tailwind CSS</h3>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-            Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order
-            E-commerce & Marketing pages.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+        {experiencesData.map((experience, index) => (
+          <motion.li
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+            className="mb-10 ml-4 rounded-lg border border-black/5 bg-gray-100 p-4 hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20"
           >
-            Learn more{' '}
-            <svg
-              className="ml-2 h-3 w-3"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
-        </li>
-        <li className="mb-10 ml-4">
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">March 2022</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Marketing UI design in Figma</h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            All of the pages and components are first designed in Figma and we keep a parity between the two versions
-            even as we update the project.
-          </p>
-        </li>
-        <li className="ml-4">
-          <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
-          <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
-          <h3 className="text-lg font-semibold text-gray-900 dark:text-white">E-Commerce UI code in Tailwind CSS</h3>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
-          </p>
-        </li>
+            <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm text-gray-700 dark:text-white/70">{experience.date}</time>
+            {/* <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{experience.project}</h3>
+            <p className="experienceText">
+              <span className="font-semibold">Description:</span>
+              {` `} {experience.description}
+            </p>
+            <p className="experienceText">
+              <span className="font-semibold">Responsibility:</span>
+              {` `} {experience.responsibility}
+            </p>
+            <p className="experienceText">
+              <span className="font-semibold">Task:</span>
+              {` `} {experience.task}
+            </p>
+            <ul className="mt-4 flex flex-wrap gap-2 py-1 sm:mt-auto">
+              {experience.relatedTechnologies.map((technologie, index) => (
+                <li
+                  className="rounded-full bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white dark:text-white/70"
+                  key={index}
+                >
+                  {technologie}
+                </li>
+              ))}
+            </ul> */}
+          </motion.li>
+        ))}
       </ol>
     </section>
   )
